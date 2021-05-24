1982ac0631d3e43592f3e0d58852c4a395cdc2b2
markrajk <marko88rajkovic@gmail.com>
2021-05-24 19:16:04 +0200

    update 01

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -115,7 +115,9 @@ const Header = ({ history }) => {
   }
 
   useEffect(() => {
-    dispatch(getUser(userInfo._id))
+    if (userInfo) {
+      dispatch(getUser(userInfo._id))
+    }
     // if (!gotUserInfo && userInfo) {
     //   // dispatch(getUser(userInfo._id))
 
@@ -312,78 +314,82 @@ const Header = ({ history }) => {
       <AppBar position="static">
         <Container maxWidth="xl">
           <Toolbar>
-            <IconButton
-              onClick={handleDrawerOpen}
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="open drawer"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography className={classes.title} variant="h6" noWrap>
-              <Link to={'/'}>
-                {gotUserInfo &&
-                [
-                  ...gotUserInfo.owner,
-                  ...gotUserInfo.admin,
-                  ...gotUserInfo.member,
-                ].find((team) => team._id === teamId)
-                  ? [
+            {userInfo && (
+              <>
+                <IconButton
+                  onClick={handleDrawerOpen}
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="open drawer"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography className={classes.title} variant="h6" noWrap>
+                  <Link to={'/'}>
+                    {gotUserInfo &&
+                    [
                       ...gotUserInfo.owner,
                       ...gotUserInfo.admin,
                       ...gotUserInfo.member,
-                    ].find((team) => team._id === teamId).name
-                  : 'Select team'}
-              </Link>
-            </Typography>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </div>
-            <div className={classes.grow} />
-            {userInfo && (
-              <>
-                <div className={classes.sectionDesktop}>
-                  <IconButton
-                    aria-label="show 17 new notifications"
-                    color="inherit"
-                  >
-                    <Badge badgeContent={17} color="secondary">
-                      <NotificationsIcon />
-                    </Badge>
-                  </IconButton>
-                  <IconButton
-                    edge="end"
-                    aria-label="account of current user"
-                    aria-controls={menuId}
-                    aria-haspopup="true"
-                    onClick={handleProfileMenuOpen}
-                    color="inherit"
-                  >
-                    <AccountCircle />
-                  </IconButton>
+                    ].find((team) => team._id === teamId)
+                      ? [
+                          ...gotUserInfo.owner,
+                          ...gotUserInfo.admin,
+                          ...gotUserInfo.member,
+                        ].find((team) => team._id === teamId).name
+                      : 'Select team'}
+                  </Link>
+                </Typography>
+                <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                    <SearchIcon />
+                  </div>
+                  <InputBase
+                    placeholder="Search…"
+                    classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                    }}
+                    inputProps={{ 'aria-label': 'search' }}
+                  />
                 </div>
-                <div className={classes.sectionMobile}>
-                  <IconButton
-                    aria-label="show more"
-                    aria-controls={mobileMenuId}
-                    aria-haspopup="true"
-                    onClick={handleMobileMenuOpen}
-                    color="inherit"
-                  >
-                    <MoreIcon />
-                  </IconButton>
-                </div>
+                <div className={classes.grow} />
+                {userInfo && (
+                  <>
+                    <div className={classes.sectionDesktop}>
+                      <IconButton
+                        aria-label="show 17 new notifications"
+                        color="inherit"
+                      >
+                        <Badge badgeContent={17} color="secondary">
+                          <NotificationsIcon />
+                        </Badge>
+                      </IconButton>
+                      <IconButton
+                        edge="end"
+                        aria-label="account of current user"
+                        aria-controls={menuId}
+                        aria-haspopup="true"
+                        onClick={handleProfileMenuOpen}
+                        color="inherit"
+                      >
+                        <AccountCircle />
+                      </IconButton>
+                    </div>
+                    <div className={classes.sectionMobile}>
+                      <IconButton
+                        aria-label="show more"
+                        aria-controls={mobileMenuId}
+                        aria-haspopup="true"
+                        onClick={handleMobileMenuOpen}
+                        color="inherit"
+                      >
+                        <MoreIcon />
+                      </IconButton>
+                    </div>
+                  </>
+                )}
               </>
             )}
           </Toolbar>
